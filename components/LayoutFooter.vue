e766cd0b15ed108b1be4b966fad6154f77a0a149
Kevin Fabre <me@kevinfabre.com>
2022-03-23 15:56:44 +0100

    Update: Footer
    Conditionnaly display event and twitter UI

--- components/LayoutFooter.vue
+++ components/LayoutFooter.vue
@@ -5,40 +5,40 @@ import { useNow } from '@vueuse/core'
 const props = defineProps({
   eventLogo: {
     type: String,
+    default: ""
   },
   eventUrl: {
     type: String,
+    default: ""
   },
   twitter: {
     type: String,
+    default: ""
   },
   twitterUrl: {
     type: String,
+    default: ""
   },
 })
 
-const { now } = useNow()
+const now = useNow()
 
 const date = computed(() => now?.value.toLocaleDateString('en-US'))
+
 </script>
 <template>
   <footer
     v-if="eventLogo || twitter"
-    class="
-      mt-auto
-      flex
-      justify-between
-      items-center
-      w-full
-      text-primary
-      dark:text-white
-      z-10
-    "
+    class="mt-auto flex items-center w-full text-primary dark:text-white z-10 justify-between"
   >
     <div class="mb-0 flex items-center">
-      <a :href="eventUrl" target="_blank"
-        ><img :src="eventLogo" class="h-8 mr-4" alt="Event image" /></a
-      ><span>{{ date }}</span>
+      <template v-if="eventLogo">
+        <a :href="eventUrl" target="_blank">
+          <img :src="eventLogo" class="h-8 mr-4" alt="Event image" />
+        </a>
+        <span>&nbsp; - &nbsp;</span>
+      </template>
+      <span>{{ date }}</span>
     </div>
     <a v-if="twitter" :href="twitterUrl" target="_blank">{{ twitter }}</a>
   </footer>
